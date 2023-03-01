830a03edbd0ea8fe52fc8c8929851900b8727e0f
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-02-28 23:11:12 -0500

    Created columns for post and comment models

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,28 @@ const sequelize = require('../config/connection');
 class Comment extends Model {};
 
 Comment.init({
+    id: {
 
-});
+    },
+    text: {
+
+    },
+    user_id: {
+
+    },
+    date_created: {
+
+    },
+    post_id: {
+
+    },
+},
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+}
+);
 
 module.exports = Comment;
--- models/post.js
+++ models/post.js
@@ -4,7 +4,28 @@ const sequelize = require('../config/connection');
 class Post extends Model {};
 
 Post.init({
+    id: {
 
-});
+    },
+    title: {
+
+    },
+    text: {
+
+    },
+    user_id: {
+
+    },
+    date_created: {
+
+    },
+},
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+}
+);
 
 module.exports = Post;
