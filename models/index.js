624fc8aa33f9d59f0af196793dc0c8753e53eae4
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-02-28 23:35:23 -0500

    Completed index model

--- models/index.js
+++ models/index.js
@@ -2,3 +2,32 @@ const Comment = require('./comment');
 const Post = require('./post');
 const User = require('./user');
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { Comment, Post, User };
