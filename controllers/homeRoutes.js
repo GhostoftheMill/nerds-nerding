efe5f3b44b58010b1549863119c2200f59378782
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-03-01 10:32:29 -0500

    Created variables for dashboard and home routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router;
+const sequelize = require('../config/connection');
+const auth = require('../utils/auth');
+const { Comment, Post, User } = require('../models');
+
+
+
+module.exports = router;
