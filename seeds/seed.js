a01921cda93219199273ccf2db9346619b2535cf
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-03-01 10:23:41 -0500

    Created index js in controllers

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,25 @@
-//dbs to seed
+const sequelize = require('../config/connection');
+const { User, Comment, Post } = require('../models');
+
+const userData = require('./userData.json');
+const projectData = require('./projectData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const project of projectData) {
+    await Post.create({
+      ...project,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
-const handlebars = require('express-handlebars');
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
+const helpers = require('.utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Right Left Two Right',
-  cookie: {},
+  cookie: {
+    maxAge: 250000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -25,9 +30,12 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const hbs = exphbs.create({ helpers });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const routes = require('./controllers');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
