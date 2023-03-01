7ce198559fcb44e14c8d93f1666b79aada0034ce
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-03-01 14:08:05 -0500

    Defined dashboard routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,4 +3,15 @@ const sequelize = require('../config/connection')
 const auth = require('../utils/auth');
 const { Comment, Post, User } = require('../models');
 
+router.get('/', async (req, res) => {
+    let posts = await Post.findAll();
+    posts = posts.map((post)=> {
+        return post.get({ plain: true });
+    })
+    console.log(posts);
+    res.render('homepage', { posts });
+});
+
+
+
 module.exports = router;
